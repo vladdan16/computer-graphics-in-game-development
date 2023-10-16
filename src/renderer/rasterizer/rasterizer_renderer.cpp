baa676a3c2111c80b9e17e7fa74ba774b88fe696
Vladislav <v.danshov@innopolis.university>
2023-10-16 20:57:47 +0300

    bonus task: add fog for object. the further object is, the more fog is applied

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -2,6 +2,28 @@
 
 #include "utils/resource_utils.h"
 
+#include <linalg.h>
+
+// this function is used to compute fog factor - the further the object is, the more fog is applied
+float computeFogFactor(float depth, float fogStart, float fogEnd) {
+	// I decided to make such change of z value for convenience
+	auto z = static_cast<float>(static_cast<int>(depth * 1000000) % 1000);
+	return linalg::clamp((fogEnd - z) / (fogEnd - fogStart), 0.0f, 1.0f);
+}
+
+// this function is used to compute color of the pixel with fog
+cg::color lerp(const cg::color& a, const cg::color& b, float t) {
+	cg::color result{};
+	result.r = a.r * (1.f - t) + b.r * t;
+	result.g = a.g * (1.f - t) + b.g * t;
+	result.b = a.b * (1.f - t) + b.b * t;
+	return result;
+}
+
+cg::color fog_color{0.0f, 0.0f, 0.0f}; // Default to black fog
+// I noticed that z value varies approximately from 0.999789 to 0.999810, so this is the reason for such values
+float fog_start = 789.0f;           // Default start of fog
+float fog_end = 810.0f;             // Default end of fog
 
 void cg::renderer::rasterization_renderer::init()
 {
@@ -41,11 +63,16 @@ void cg::renderer::rasterization_renderer::render()
 		return std::make_pair(processed, data);
 	};
 	rasterizer->pixel_shader = [](const cg::vertex& data, const float z) {
-		return cg::color{
+		cg::color original_color{
 				data.ambient_r,
 				data.ambient_g,
 				data.ambient_b,
 		};
+
+		float fogFactor = computeFogFactor(z, fog_start, fog_end);
+		cg::color fogged_color = lerp(original_color, fog_color, (1.f - fogFactor) / 2);
+
+		return fogged_color;
 	};
 
 	auto start = std::chrono::high_resolution_clock::now();
